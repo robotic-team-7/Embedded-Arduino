6aadca199a7cbace6db632fe439fb39d46036735
azaqis <55734899+azaqis@users.noreply.github.com>
2022-04-07 12:57:51 +0200

    Added simple gyroscope code

--- main/main.ino
+++ main/main.ino
@@ -11,6 +11,7 @@ MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 MeRGBLed rgbled_0(0, 12);
 MeLineFollower linefollower_9(9);
+MeGyro gyro(0, 0x69);
 
 //Adjust brightness of LED-ring. 0.0 = minimum brightness, 1.0 = maximum brightness
 float led_brightness = 0.1;
@@ -68,6 +69,8 @@ void _delay(float seconds) {
 
 void setup() {
   Serial.begin(9600);
+
+  gyro.begin();
   
   randomSeed((unsigned long)(lightsensor_12.read() * 123456));
   TCCR1A = _BV(WGM10);
@@ -89,7 +92,7 @@ void setup() {
         //Show blue color on LED-ring
         rgbled_0.setColor(0,ledBlue[0],ledBlue[1],ledBlue[2]);
         rgbled_0.show();
-        
+
         //Move backwards in 0.5 seconds, 50% of maximum speed
         move(2, 50 / 100.0 * 255);
         _delay(0.5);
@@ -110,7 +113,8 @@ void setup() {
             }
           }
         }
-        
+
+        //Choose left or right randomly and turn in  1 second 50% of speed
         int randomDirection = rand() % 2 + 3;
         move(randomDirection, 50 / 100.0 * 255);
         _delay(1);
@@ -131,7 +135,8 @@ void setup() {
         move(2, 50 / 100.0 * 255);
         _delay(0.5);
         move(2, 0);
-        
+
+        //Choose left or right randomly and turn in  1 second 50% of speed
         int randomDirection = rand() % 2 + 3;
         move(randomDirection, 50 / 100.0 * 255);
         _delay(1);
@@ -144,17 +149,36 @@ void setup() {
 
         //Go forward, 50% of maximum speed
         move(1, 50 / 100.0 * 255);
-
       }
-
+  
       _loop();
   }
+
+}
+
+//For debug purpose
+void print_gyro_values(){
+        Serial.print("X: ");
+        Serial.print(gyro.getAngle(1));
+        Serial.print("Y: ");
+        Serial.print(gyro.getAngle(2));
+        Serial.print("Z: ");
+        Serial.print(gyro.getAngle(3));
+        Serial.print("\n");
+        Serial.print("X GYRO: ");
+        Serial.print(gyro.getGyroX());
+        Serial.print("Y GYRO: ");
+        Serial.print(gyro.getGyroY());
+        Serial.print("\n");
+        Serial.print("\n");
 
 }
 
 void _loop() {
   Encoder_1.loop();
   Encoder_2.loop();
+  gyro.update();
+  //print_gyro_values(); For debug purpose
 }
 
 void loop() {
