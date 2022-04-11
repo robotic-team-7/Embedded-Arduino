56b017609c35e5245dcf7ba40caa452f8c951637
azaqis <55734899+azaqis@users.noreply.github.com>
2022-04-11 09:35:59 +0200

    Structured up and added states

--- main/main.ino
+++ main/main.ino
@@ -21,6 +21,13 @@ int ledRed[3] = {(int)(255 * led_brightness), 0, 0};
 int ledGreen[3] = {0, (int)(255 * led_brightness), 0};
 int ledBlue[3] = {0, 0, (int)(255 * led_brightness)};
 
+typedef enum{
+  S_AUTO,
+  S_MANUAL
+} s_modes;
+
+s_modes mode = S_AUTO;
+
 void isr_process_encoder1(void)
 {
   if(digitalRead(Encoder_1.getPortB()) == 0){
@@ -67,6 +74,22 @@ void _delay(float seconds) {
   while(millis() < endTime) _loop();
 }
 
+void serialCheckState(){
+  if(Serial.available() > 0){
+    int availableSerial = Serial.available();
+    char buff[availableSerial];
+    Serial.readString().toCharArray(buff, availableSerial + 1);
+  
+    if(strcmp(buff, "auto") == 0){
+      mode = S_AUTO;
+    }
+    else if(strcmp(buff, "manual") == 0){
+      mode = S_MANUAL;
+    }
+    
+  }
+}
+
 void setup() {
   Serial.begin(9600);
 
@@ -83,94 +106,117 @@ void setup() {
   rgbled_0.fillPixelsBak(0, 2, 1);
   
   while(1) {
-      if(ultrasonic_10.distanceCm() < 20){
-        //Set motor speed to 0 in 0.5 seconds
-        Encoder_1.setTarPWM(0);
-        Encoder_2.setTarPWM(0);
-        _delay(0.5);
-        
-        //Show blue color on LED-ring
-        rgbled_0.setColor(0,ledBlue[0],ledBlue[1],ledBlue[2]);
-        rgbled_0.show();
-
-        //Move backwards in 0.5 seconds, 50% of maximum speed
-        move(2, 50 / 100.0 * 255);
-        _delay(0.5);
-        move(2, 0);
-
-        //Notify Raspberry Pi that Ultrasonic sensor has observed an obsticle 
-       Serial.write("hit");
-       _delay(1); //Might not needed
-       //Await response from Raspberry Pi that picture is captured
-        while(1){
-          if(Serial.available() == 4){
-            int availableSerial = Serial.available();
-            char buff[availableSerial];
-            Serial.readString().toCharArray(buff, availableSerial + 1);
-
-            if(strcmp(buff, "done") == 0){
-              break;
-            }
-          }
-        }
-
-        //Choose left or right randomly and turn in  1 second 50% of speed
-        int randomDirection = rand() % 2 + 3;
-        move(randomDirection, 50 / 100.0 * 255);
-        _delay(1);
-        move(randomDirection, 0);
-
-      }
-      else if(linefollower_9.readSensors() == 0.000000){
-        //Set motor speed to 0 in 0.5 seconds
-        Encoder_1.setTarPWM(0);
-        Encoder_2.setTarPWM(0);
-        _delay(0.5);
-
-        //Show red color on LED-ring
-        rgbled_0.setColor(0,ledRed[0],ledRed[1],ledRed[2]);
-        rgbled_0.show();
-
-        //Go backwards in 0.5 seconds, 50% of maximum speed
-        move(2, 50 / 100.0 * 255);
-        _delay(0.5);
-        move(2, 0);
-
-        //Choose left or right randomly and turn in  1 second 50% of speed
-        int randomDirection = rand() % 2 + 3;
-        move(randomDirection, 50 / 100.0 * 255);
-        _delay(1);
-        move(randomDirection, 0);
-
-      }else{
-        //Show green color on LED-ring
-        rgbled_0.setColor(0,ledGreen[0],ledGreen[1],ledGreen[2]);
-        rgbled_0.show();
-
-        //Go forward, 50% of maximum speed
-        move(1, 50 / 100.0 * 255);
-      }
-  
-      _loop();
+    serialCheckState();
+    if(mode == S_AUTO){
+      autoMode();
+    }
+    else if(mode == S_MANUAL){
+      manualMode();
+    }
+    _loop();
   }
+}
 
+void autoMode(){
+  if(ultrasonic_10.distanceCm() < 20){
+    ultrasonicTriggered();
+  }
+  else if(linefollower_9.readSensors() == 0.000000){
+    lineFollowerTriggered();
+
+  }else{
+    autoDriveForward();   
+  }
+}
+
+void manualMode(){
+  
+}
+
+void ultrasonicTriggered(){
+  //Set motor speed to 0 in 0.5 seconds
+  Encoder_1.setTarPWM(0);
+  Encoder_2.setTarPWM(0);
+  _delay(0.5);
+  
+  //Show blue color on LED-ring
+  rgbled_0.setColor(0,ledBlue[0],ledBlue[1],ledBlue[2]);
+  rgbled_0.show();
+  
+  //Move backwards in 0.5 seconds, 50% of maximum speed
+  move(2, 50 / 100.0 * 255);
+  _delay(0.5);
+  move(2, 0);
+  
+  //Notify Raspberry Pi that Ultrasonic sensor has observed an obsticle 
+  Serial.write("hit");
+  _delay(1); //Might not needed
+  //Await response from Raspberry Pi that picture is captured
+  while(1){
+    if(Serial.available() == 4){
+      int availableSerial = Serial.available();
+      char buff[availableSerial];
+      Serial.readString().toCharArray(buff, availableSerial + 1);
+  
+      if(strcmp(buff, "done") == 0){
+        break;
+      }
+    }
+  }
+  
+  //Choose left or right randomly and turn in  1 second 50% of speed
+  int randomDirection = rand() % 2 + 3;
+  move(randomDirection, 50 / 100.0 * 255);
+  _delay(1);
+  move(randomDirection, 0);
+}
+
+void lineFollowerTriggered(){
+  //Set motor speed to 0 in 0.5 seconds
+  Encoder_1.setTarPWM(0);
+  Encoder_2.setTarPWM(0);
+  _delay(0.5);
+  
+  //Show red color on LED-ring
+  rgbled_0.setColor(0,ledRed[0],ledRed[1],ledRed[2]);
+  rgbled_0.show();
+  
+  //Go backwards in 0.5 seconds, 50% of maximum speed
+  move(2, 50 / 100.0 * 255);
+  _delay(0.5);
+  move(2, 0);
+  
+  //Choose left or right randomly and turn in  1 second 50% of speed
+  int randomDirection = rand() % 2 + 3;
+  move(randomDirection, 50 / 100.0 * 255);
+  _delay(1);
+  move(randomDirection, 0);
+}
+
+void autoDriveForward(){
+  //Show green color on LED-ring
+  rgbled_0.setColor(0,ledGreen[0],ledGreen[1],ledGreen[2]);
+  rgbled_0.show();
+  
+  //Go forward, 50% of maximum speed
+  move(1, 50 / 100.0 * 255);
 }
 
 //For debug purpose
 void print_gyro_values(){
-        Serial.print("X: ");
-        Serial.print(gyro.getAngle(1));
-        Serial.print("Y: ");
-        Serial.print(gyro.getAngle(2));
-        Serial.print("Z: ");
-        Serial.print(gyro.getAngle(3));
-        Serial.print("\n");
-        Serial.print("X GYRO: ");
-        Serial.print(gyro.getGyroX());
-        Serial.print("Y GYRO: ");
-        Serial.print(gyro.getGyroY());
-        Serial.print("\n");
-        Serial.print("\n");
+  Serial.print("X: ");
+  Serial.print(gyro.getAngle(1));
+  Serial.print("Y: ");
+  Serial.print(gyro.getAngle(2));
+  Serial.print("Z: ");
+  Serial.print(gyro.getAngle(3));
+  Serial.print("\n");
+  Serial.print("X GYRO: ");
+  Serial.print(gyro.getGyroX());
+  Serial.print("Y GYRO: ");
+  Serial.print(gyro.getGyroY());
+  Serial.print("\n");
+  Serial.print("\n");
 
 }
 
